0c3119885325b0461090538961730bcdb389e27a
xuansontrinh <xuanson.trinh@outlook.com>
2016-12-19 11:55:11 +0700

    Solution init and EEA implemented

---
+++ EEA/EEA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace EEA
+{
+
+    public class Helper
+    {
+        public Helper()
+        {
+
+        }
+
+        public List<Tuple<BigInteger, BigInteger, BigInteger>> EEA(BigInteger a, BigInteger b)
+        {
+
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
