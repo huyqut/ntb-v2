d8efcaa09317a0bdf489cb05893e0deb8e16a7c3
xuansontrinh <xuanson.trinh@outlook.com>
2016-12-19 14:26:55 +0700

    Fix EEA implementation

--- EEA/EEA/Program.cs
+++ EEA/EEA/Program.cs
@@ -16,23 +16,29 @@ namespace EEA
 
         public List<Tuple<BigInteger, BigInteger, BigInteger>> EEA(BigInteger a, BigInteger b)
         {
+            List<Tuple<BigInteger, BigInteger, BigInteger>> ret = new List<Tuple<BigInteger, BigInteger, BigInteger>>();
             BigInteger r = a;
             BigInteger r_1 = b;
             BigInteger s = 1;
             BigInteger s_1 = 0;
             BigInteger t = 0;
             BigInteger t_1 = 1;
-            List<Tuple<BigInteger, BigInteger, BigInteger>> ret = new List<Tuple<BigInteger, BigInteger, BigInteger>>();
+            ret.Add(new Tuple<BigInteger, BigInteger, BigInteger>(r, s, t));
             while (!r_1.IsZero)
             {
                 BigInteger q = r / r_1;
                 BigInteger r_2 = r % r_1;
                 r = r_1;
+                r_1 = r_2;
+                BigInteger temp_s = s;
+                BigInteger temp_t = t;
                 s = s_1;
                 t = t_1;
-                r_1 = r_2;
-                s_1 = s - s_1 * q;
-                t_1 = t - t_1 * q;
+                s_1 = temp_s - s_1 * q;
+                t_1 = temp_t - t_1 * q;
+
+                
+
                 ret.Add(new Tuple<BigInteger, BigInteger, BigInteger>(r, s, t));
             }
             return ret;
